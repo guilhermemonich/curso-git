7a602bff2306dc38275b27f1bd34c6afd6983b8d
Guilherme Monich <guilherme.monich@gmail.com>
2020-01-08 10:43:16 -0300

    Primeira versão do projeto

---
+++ ExercicioSecao05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercicioSecao05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdAlunos;
+            HashSet<int> alunos = new HashSet<int>();
+            HashSet<int> alunosA = new HashSet<int>();
+            HashSet<int> alunosB = new HashSet<int>();
+            HashSet<int> alunosC = new HashSet<int>();
+
+            Console.Write("O curso A possui quantos alunos? ");
+            qtdAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso A:");
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+                alunosA.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("O curso B possui quantos alunos? ");
+            qtdAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso B:");
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+                alunosB.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.Write("O curso C possui quantos alunos? ");
+            qtdAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os códigos dos alunos do curso C:");
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+                alunosC.Add(int.Parse(Console.ReadLine()));
+            }
+
+            alunos.UnionWith(alunosA);
+            alunos.UnionWith(alunosB);
+            alunos.UnionWith(alunosC);
+
+            Console.Write($"Total de alunos: { alunos.Count}");
+            Console.ReadKey();
+        }
+    }
+}
